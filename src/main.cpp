43266f16aed793a807b6d48f3545027f14eac9e7
Steel Ricciotti <steel.ricciotti@gmail.com>
2025-03-15 14:53:16 -0400

    Created data model and implemented billing batch and overdue batch

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <iomanip> 
+#include "batch.h"
+
+using namespace std;
+using namespace std::chrono;
+
+
+int main(){
+
+    BatchService service;
+    //service.BillingBatch(1);
+    service.CalculateOverdue();
+    
+    return 0;
+}
